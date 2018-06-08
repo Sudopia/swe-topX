4e7d6de731593d32a9ed689f4fa6f835f4532aff
sudopia <prestonruff@gmail.com>
2018-06-08 15:03:59 -0400

    Refactor into atomic functions

--- jsontest.py
+++ jsontest.py
@@ -6,10 +6,24 @@
 # Organization: IU: CACR
 # Filename: jsontest.py
 # Date: 06/05/2018
+# Todo: make better variable names
+#       match code to google python style guideline
 ##
 
+#the number of CWE's is determined by the number of desciption fields present
+def checkMultiCWE(b):
+        if b > 1:
+            print(cve_id,"has",b,"CWE's")
+    
+#the number of CWE's is determined by the number of desciption fields present
+def checkNoCWE(b):
+        if b == 0:
+            print(cve_id,"has",b,"CWE's")
+
 import json
-with open('CVE-Modified/nvdcve-1.0-modified.json', 'r') as fp:
+#../CVE-Modified/nvdcve-1.0-modified.json
+#../CVE-2017/nvdcve-1.0-2017.json
+with open('../CVE-2017/nvdcve-1.0-2017.json', 'r') as fp:
     try:
         obj = json.load(fp)
     except ValueError:
@@ -26,6 +40,8 @@ for item in obj['CVE_Items']:
         for item in cwe:
             b += 1
             #print(item['value'])
-        if b != 1:
-            print(cve_id,"has",b,"description fields")
+
+        # uncomment to run desired functions below
+        checkMultiCWE(b) 
+        #checkNoCWE(b)
         b = 0
