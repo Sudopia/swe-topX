eec21849ce0b3e849242e4b393ee67f5dee43e39
sudopia <prestonruff@gmail.com>
2018-06-18 14:52:47 -0400

    Remove braces from filtered NVD feed

---
+++ rmBraces.py
@@ -0,0 +1,26 @@
+#! bin/sh/ python
+#!/usr/bin/env python
+
+##
+# Author: Preston Ruff
+# Filename: rmBraces.py
+# Todo: make better variable names
+#       match code to google python style guideline
+##
+
+import re, fileinput
+from os import replace
+
+fName = 'edited-nvdcve-1.0-2017.json'
+fNametmp = fName + ".tmp"
+fp = open(fName, 'r+')
+fpTmp = open(fNametmp, 'w')
+
+for line in fp:
+    if not re.search(r'^ +\{\},' "\n",line):
+        fpTmp.write(line)
+
+fp.close()
+fpTmp.close()
+replace(fNametmp, fName)
+
