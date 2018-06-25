baf8cfddb64379f9172604b746d05bf12ff553dc
sudopia <prestonruff@gmail.com>
2018-06-25 14:00:40 -0400

    Optimized CWE frequency count by presorting CWEs
    The previous attempt at an algorithm was going to be too messy.
    This method not only actually works this time but is simpler because
    I do not have to modify the raw source list of CWE's where there
    are as many non-unique CWE lines as the frequency that a CWE-ID occured.
    I simply compare a temporary variable to the value of the next line in
    the source file and increment a counter each time a see a duplicate
    line. When a line differs from the temporary variable value, you know
    that you are reading a different word, so you may reset and restart the
    incrementing of your frequency counter.

--- countCWE.py
+++ countCWE.py
@@ -43,80 +43,108 @@ def plotFreq(freq, cweId):
 
     plt.show()
 
-def sumFreq(fPath):
-    with open(fPath, 'r+') as fpA:
-        readerA = csv.reader(fpA, delimiter=',')
-        writerA = csv.writer(fpA)
-        with open(fPath, 'r+') as fpB:
-            readerB = csv.reader(fpB, delimiter=',')
-            #test to see what happens if I edit the file with one filepointer while the other filepointer is reading below its location in the file
-            #am I even allowed to open the file in more than one location,
-            #I can't remember what happens to fp's when you make edits from multiple locations at the same time
-            #I will have to go through the file again and remove the lines which are equal to '\n'
-            #the only way to do this will be to omit writing those lines out as I read in the rows of the csv file
-            #then I have to go through the file again and sort the file by highest frequency
-            #I will just save each non-nl line to a tuple and feed it to a sorting function
-            #then I will overwrite the csv lines with the sorted list/tuple
-            #since I will need to put the items into a list anyway, I should just maintain a list from the main function, wait to sort until I confirm that I am done adding new ID elements,
-            #then call the sumFreq() function to sum the elements and sort them using a pointer to the list
-            #research seems to suggest that passing large objects (i.e. a list) as a parameter to a function are not especially inefficient as they are in C unless specifically done by reference (a pointer)
-            #then call writeOut() to actually write my completed work to the CSV file
-            c = 0
-            length = 0
-            for row in readerA:
-                length = length + 1
-            fpA.seek(0,0)
-            for row in readerA:
-                if c != 0: #in order to "edit" a csv file, it looks like you have to rewrite the whole file but omit or rewrite lines which do not meet your criteria
-                    #row[1] = "ayylmao"
-                    fpA.write('')
-                c = c + 1
-            for row in readerB:
-                print(row)
-            
+#def sumFreq(cweList):
+def sumFreq(cweList):
+    
+        #cweList = ["jam","jam","cat","dog","dog"]
+        #print("init cweList:",cweList)
+        cweList.sort()
+        #print("sorted cweList:",cweList)
+        
+        firstLine = True
 
-            '''
-            c = 0
-            length = 0
-            for row in reader:
-                length = length + 1
-            while c <= length
-                for row in reader:
-                    if i == 0:
-                    curID = row[0]
-                    curFreq = row[1]
+        idList = list()
+        freqList = list()
+        freq = 0
+        loopNum = 0
+        lastFreq = 0
+        tmp = ""
+        for line in cweList:
+            if firstLine == True:
+                tmp = line
+                freq = 1
+                firstLine = False
+            else:
+                if line == tmp:
+                    freq = freq + 1
+                else:
+                    idList.append(tmp)
+                    freqList.append(freq)
+                    tmp = line
+                    freq = 1
+            lastFreq = freq
+            #print("loopNum:",loopNum)
+            #print("\ttmp:",tmp)
+            #print("\tline:",line)
+            #print("\tfirstLine:",firstLine)
+            loopNum = loopNum + 1
+        idList.append(line)
+        freqList.append(lastFreq)
+        #print("idList:",idList)
+        #print("freqList:",freqList)
+        
+        freqList, idList = zip(*sorted(zip(freqList, idList), reverse=True))
+        return (freqList, idList)
 
-                    c = c + 1
-            c = 0
-                    break
+        '''
+        cweList = ["cat","jam","cat","dog","dog"]
+        cweList.sort()
+        print("cweList:",cweList)
+        count = 0
+        freqList = list() 
+        idList = list()
+        print("A: printing freqList")
+        print(freqList)
+        print("A: end printing freqList")
+        #minimize CWE-ID reference duplicates and increment their frequency count
+        tmp = 0
+        while cweList:
+            firstLine = True
+            for x in cweList:
+                if firstLine == True:
+                    tmp = x
+                    count = count + 1
+                    cweList.remove(x)
+                elif x == tmp:
+                    count = count + 1
+                    cweList.remove(x)
+                print("cweList after remove(x):")
+                print(cweList)
+                if (tmp not in cweList) and (tmp != 0):
+                    freqList.append(count)
+                    idList.append(tmp)
+                    count = 0
+                firstLine = False
+            tmp = 0
+        print("B: printing freqList")
+        print(freqList)
+        print("B: end printing freqList")
+        #sorts both lists together using the frequency list
+        freqList, idList = zip(*sorted(zip(freqList, idList), reverse=True))
+        print("printing idList, freqlist")
+        for i,val in enumerate(idList):
+            print(val,freqList[i])
+        return (freqList, idList)
+        '''
+
+def psuedoSort(cweList):
+    cweList.sort()
+    '''
+        1. presort the listA
+        2. add unique IDs to listB
+        3. add associated frequencies to listC
+        4. loop to write all B-C pairs to output file
+    '''
 
 
-    row = str()
-    count = 0
-    freqList = list() 
-    idList = list()
-
-    while cweList:
-        for x in cweList:
-            tmp = x
-            count = count + 1
-            cweList.remove(x)
-            if tmp not in cweList:
-                freqList.append(count)
-                idList.append(tmp)
-                count = 0
-    if fp == fileInList[0] and args.writemethod == 'w':
-        csvFile = open(fileOut, 'w', newline='')
-    else:
-        csvFile = open(fileOut, 'a', newline='')
-'''
+    return 0
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("outputfile", help="Output filename for CSV file.")
     parser.add_argument("writemethod", choices=['w','a'], help="Output file write method:'w' flag to overwrite existing file or create a new file.'a' flag to append to existing file.")
-    parser.add_argument("inputfile", nargs='+', help="Input filename for JSON file. To parse multiple JSON files, write the '-i' option before each filename.")
+    parser.add_argument("inputfile", nargs='+', help="Input filename(s) for JSON input files (separate names by a space).")
     if not len(sys.argv) > 1:
         parser.print_help()
         sys.exit()
@@ -126,8 +154,6 @@ def main():
             print("Write Method Error: cannot use 'a' option if outputfile does not exist")
             print("Hint: change 'a' flag to 'w' flag to create a new output file (or overwrite an existing file!)")
             sys.exit()
-    #print("read the inputfile args:", args.inputfile)
-    #print("read the outputfile args:", args.outputfile)
 
     fileInList = args.inputfile
     fileOut = args.outputfile
@@ -135,6 +161,7 @@ def main():
     #fileIn = 'edited-nvdcve-1.0-2017.json'
     #fileOut = "sortedCWE.csv"
 
+    cweList = list()
     #save individual CWE-ID references
     for fp in fileInList:
         with open(fp, 'r') as fpIn:
@@ -142,57 +169,35 @@ def main():
                 obj = json.load(fpIn)
             except ValueError:
                 print("error loading JSON")
-        cweList = list()
         for item in obj['CVE_Items']:
-            try:
                 cve = item['cve']
                 probtype = cve['problemtype']
                 for field in probtype['problemtype_data']:
                     cwe = field['description']
                     for el in cwe:
                         cweList.append(el['value'])
-            except:
-                pass
+    row = str()
+    count = 0
+    freqList = list() 
+    idList = list()
+   
+    #a tuple containing two list elements
+    freqList = sumFreq(cweList)
+    idList = freqList[1]
+    freqList = freqList[0]
 
-        row = str()
-        count = 0
-        freqList = list() 
-        idList = list()
-
-        #minimize CWE-ID reference duplicates and increment their frequency count
-        while cweList:
-            for x in cweList:
-                tmp = x
-                count = count + 1
-                cweList.remove(x)
-                if tmp not in cweList:
-                    freqList.append(count)
-                    idList.append(tmp)
-                    count = 0
-
-        if fp == fileInList[0] and args.writemethod == 'w':
-            csvFile = open(fileOut, 'w', newline='')
-        else:
-            csvFile = open(fileOut, 'a', newline='')
-
-        #side effect: converts lists to tuples when sorting the lists using a shared index
-        freqList, idList = zip(*sorted(zip(freqList, idList), reverse=True))
-        freqList = list(freqList)
-        #plotFreq(freqList, idList)
-        #csv.write("CWE ID" + "," + "Frequency\n")
-        writer = csv.writer(csvFile)
-        if fp == fileInList[0]:
-            writer.writerow(("CWE ID", "Frequency"))
-        for i, val in enumerate(freqList):
-            #text = str(val) + idList[i]
-            #sortedFile.write(text)
-            row = idList[i], str(val)
-            writer.writerow(row)
-            #writer.writerows([[str(val)], [idList[i]]])
-    if(len(fileInList) > 1):
-        csvFile.close()
-        sumFreq(fileOut)
+    if args.writemethod == 'w':
+        csvFile = open(fileOut, 'w', newline='')
     else:
-        csvFile.close()
+        csvFile = open(fileOut, 'a', newline='')
+
+    writer = csv.writer(csvFile)
+    if args.writemethod == 'w':
+        writer.writerow(("CWE ID", "Frequency"))
+    for i, val in enumerate(freqList):
+        row = idList[i], str(val)
+        writer.writerow(row)
+    #plotFreq(freqList, idList)
+    csvFile.close()
 if __name__ == "__main__":
     main()
